a024a14b1cd64d6b6b29903d5f5291b0052e0dc1
ThanViet <tvviet.25174600182@sv.uneti.edu.vn>
2025-12-28 16:59:08 +0700

    nộp bài tập chương 12

---
+++ bai1C12_main.py
@@ -0,0 +1,10 @@
+# main.py
+from bai1C12_hinh_hoc import chu_vi_hinh_vuong, chu_vi_hinh_tron
+
+# Tính chu vi
+cv_hv = chu_vi_hinh_vuong(5)
+cv_ht = chu_vi_hinh_tron(3)
+
+# In kết quả
+print("Chu vi hình vuông cạnh 5 là:", cv_hv)
+print("Chu vi hình tròn bán kính 3 là:", cv_ht)
---
+++ bai2C12_main.py
@@ -0,0 +1,8 @@
+from bai2C12_chuoi_utility import dao_nguoc_chuoi
+
+chuoi = "HAPPY NEW YEAR"
+
+ket_qua = dao_nguoc_chuoi(chuoi)
+
+print("Chuỗi ban đầu:", chuoi)
+print("Chuỗi sau khi đảo ngược:", ket_qua)
